8a156be2c1bd68b963edac59de50e2cfd435bf26
yomajo <ed.vinas@yahoo.com>
2020-06-05 10:40:09 +0300

    closes #7: -simplified txt data loading -removed filter_id and its dependencies -changed logging lvl

--- Helper Files/main_amazon.py
+++ Helper Files/main_amazon.py
@@ -11,58 +11,39 @@ import os
 
 # GLOBAL VARIABLES
 TESTING = False
-EXPECTED_SYS_ARGS = 3
+EXPECTED_SYS_ARGS = 2
 VBA_ERROR_ALERT = 'ERROR_CALL_DADDY'
+VBA_KEYERROR_ALERT = 'ERROR_IN_SOURCE_HEADERS'
 VBA_OK = 'EXPORTED_SUCCESSFULLY'
 TEST_AMZN_EXPORT_TXT = r'C:\Coding\Amazon Orders Parser\Amazon exports\run1.txt'
 
 # Logging config:
 log_path = os.path.join(get_output_dir(client_file=False), 'loading_amazon_orders.log')
-logging.basicConfig(handlers=[logging.FileHandler(log_path, 'a', 'utf-8')], level=logging.DEBUG)
+logging.basicConfig(handlers=[logging.FileHandler(log_path, 'a', 'utf-8')], level=logging.INFO)
 
 
-def get_list_of_order_dicts(source_file, filter_order_id):
-    orders = []
-    txt_headers, txt_orders_data = parse_txt_file(source_file)
-    try:
-        for idx, order_row_data in enumerate(txt_orders_data):
-            single_order_list = order_row_data.strip().split('\t')
-            order_data_dict = single_order_data_to_dict(txt_headers, single_order_list)
-            if order_data_dict['order-id'] == filter_order_id:
-                logging.info(f'Found filtering ID: {filter_order_id}, filtering out {len(order_data_dict)}/{len(txt_orders_data)} orders')
-                orders.clear()
-                continue
-            orders.append(order_data_dict)
-        return orders
-    except Exception as e:
-        print(VBA_ERROR_ALERT)
-        logging.critical(f'Error reading and splitting data {e}. Provided file: {source_file}. Error encountered in {idx}/{len(txt_orders_data)} orders')
-        sys.exit()
+def get_cleaned_orders(source_file:str) -> list:
+    '''returns cleaned orders (as cleaned in clean_orders func) from source_file arg path'''
+    raw_orders = get_raw_orders(source_file)
+    cleaned_orders = clean_orders(raw_orders)
+    return cleaned_orders
 
-def single_order_data_to_dict(txt_headers, single_order_list):
-    order_data_dict = {}
-    try:
-        for txt_header, data_value in zip(txt_headers, single_order_list):
-            order_data_dict[txt_header] = data_value
-            # Replace telephone number plus with 00 for txt_header: 'buyer-phone-number'
-            if txt_header == 'buyer-phone-number':
-                order_data_dict[txt_header] = data_value.replace('+', '00')
-        return order_data_dict
-    except Exception as e:
-        print(VBA_ERROR_ALERT)
-        logging.critical(f'Error creating dict for single data row {e}. Collected dict thus far: {order_data_dict}')
-        sys.exit()
+def get_raw_orders(source_file:str) -> list:
+    '''returns raw orders as list of dicts for each order in txt source_file'''
+    with open(source_file, 'r', encoding='utf-8') as f:
+        source_contents = csv.DictReader(f, delimiter='\t')
+        raw_orders = [{header : value for header, value in row.items()} for row in source_contents]
+    return raw_orders
 
-def parse_txt_file(data_file):
-    try:
-        with open(data_file, 'r', encoding='utf-8') as f:
-            txt_headers = f.readline().strip().split('\t')
-            txt_data = f.readlines()
-        return txt_headers, txt_data
-    except Exception as e:
-        print(VBA_ERROR_ALERT)
-        logging.critical(f'Error reading txt source {e}')
-        sys.exit()
+def clean_orders(orders:list) -> list:
+    '''replaces plus sign in phone numbers with 00'''
+    for order in orders:
+        try:
+            order['buyer-phone-number'] = order['buyer-phone-number'].replace('+', '00')
+        except KeyError as e:
+            logging.warning(f'New header in source file. Alert VBA on new header. Error: {e}')
+            print(VBA_KEYERROR_ALERT)
+    return orders
 
 def parse_export_orders(testing:bool, cleaned_source_orders:list, loaded_txt:str):
     '''interacts with classes (ParseOrders, OrdersDB) to filter new orders, export desired files and push new orders to db'''
@@ -73,9 +54,9 @@ def parse_export_orders(testing:bool, cleaned_source_orders:list, loaded_txt:str
 
 def parse_args():
     if len(sys.argv) == EXPECTED_SYS_ARGS:
-        txt_path, filter_order_id = sys.argv[1], sys.argv[2]
-        logging.info(f'Accepted sys args on launch: txt_path: {txt_path}, filter_order_id: {filter_order_id}')
-        return txt_path, filter_order_id
+        txt_path = sys.argv[1]
+        logging.info(f'Accepted sys args on launch: txt_path: {txt_path}')
+        return txt_path
     else:
         print(VBA_ERROR_ALERT)
         logging.critical(f'Error parsing arguments on script initialization in cmd. Arguments provided: {len(sys.argv)} Expected: {EXPECTED_SYS_ARGS}')
@@ -83,15 +64,15 @@ def parse_args():
 
 def main(testing, amazon_export_txt_path):
     '''Main function executing parsing of provided txt file and outputing csv, xlsx files'''
-    logging.info(f'\n NEW RUN STARTING: {datetime.today().strftime("%Y.%m.%d %H:%M")}')
+    logging.info(f'\n NEW RUN STARTING: {datetime.today().strftime("%Y.%m.%d %H:%M")}')    
     if not testing:
-        txt_path, filter_order_id = parse_args()
+        txt_path = parse_args()
     else:
         print('RUNNING IN TESTING MODE')
-        txt_path, filter_order_id = amazon_export_txt_path, ''  #, '305-1937192-5680315'
+        txt_path = amazon_export_txt_path
     if os.path.exists(txt_path):
         logging.info('file exists, continuing to processing...')
-        cleaned_source_orders = get_list_of_order_dicts(txt_path, filter_order_id)
+        cleaned_source_orders = get_cleaned_orders(txt_path)
         parse_export_orders(testing, cleaned_source_orders, txt_path)
         print(VBA_OK)
     else:
