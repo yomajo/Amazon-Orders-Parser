213a372736af5b703b99be73c04aecd12aae0b69
yomajo <ed.vinas@yahoo.com>
2021-01-08 13:14:20 +0200

    accepting skip_etonas sys.arg from vba; reformatted parse_arg func

--- Helper Files/main_amazon.py
+++ Helper Files/main_amazon.py
@@ -11,8 +11,7 @@ import os
 
 # GLOBAL VARIABLES
 TESTING = False
-SKIP_ETONAS = True
-EXPECTED_SYS_ARGS = 2
+EXPECTED_SYS_ARGS = 3
 VBA_ERROR_ALERT = 'ERROR_CALL_DADDY'
 VBA_KEYERROR_ALERT = 'ERROR_IN_SOURCE_HEADERS'
 VBA_OK = 'EXPORTED_SUCCESSFULLY'
@@ -50,35 +49,38 @@ def clean_orders(orders:list) -> list:
             print(VBA_KEYERROR_ALERT)
     return orders
 
-def parse_export_orders(testing:bool, cleaned_source_orders:list, loaded_txt:str):
+def parse_export_orders(testing:bool, skip_etonas:bool, cleaned_source_orders:list, loaded_txt:str):
     '''interacts with classes (ParseOrders, OrdersDB) to filter new orders, export desired files and push new orders to db'''
     db_client = OrdersDB(cleaned_source_orders, loaded_txt)
     new_orders = db_client.get_new_orders_only()
     logging.info(f'Loaded txt contains: {len(cleaned_source_orders)}. Further processing: {len(new_orders)} orders')
-    ParseOrders(new_orders, db_client).export_orders(testing=testing, skip_etonas=SKIP_ETONAS)
+    ParseOrders(new_orders, db_client).export_orders(testing=testing, skip_etonas=skip_etonas)
 
 def parse_args():
-    if len(sys.argv) == EXPECTED_SYS_ARGS:
+    '''returns arguments passed from VBA'''
+    try:
+        assert len(sys.argv) == EXPECTED_SYS_ARGS, 'Unexpected number of sys.args passed'
         txt_path = sys.argv[1]
-        logging.info(f'Accepted sys args on launch: txt_path: {txt_path}')
-        return txt_path
-    else:
+        skip_etonas = True if sys.argv[2] == 'True' else False
+        logging.info(f'Accepted sys args on launch: txt_path: {txt_path}; skip_etonas: {skip_etonas}. Whole sys.argv: {list(sys.argv)}')
+        return txt_path, skip_etonas
+    except Exception as e:
         print(VBA_ERROR_ALERT)
-        logging.critical(f'Error parsing arguments on script initialization in cmd. Arguments provided: {len(sys.argv)} Expected: {EXPECTED_SYS_ARGS}')
+        logging.critical(f'Error parsing arguments on script initialization in cmd. Arguments provided: {list(sys.argv)} Number Expected: {EXPECTED_SYS_ARGS}.')
         sys.exit()
 
 def main(testing, amazon_export_txt_path):
     '''Main function executing parsing of provided txt file and outputing csv, xlsx files'''
     logging.info(f'\n NEW RUN STARTING: {datetime.today().strftime("%Y.%m.%d %H:%M")}')    
     if not testing:
-        txt_path = parse_args()
+        txt_path, skip_etonas = parse_args()
     else:
         print('RUNNING IN TESTING MODE')
         txt_path = amazon_export_txt_path
     if os.path.exists(txt_path):
         logging.info('file exists, continuing to processing...')
         cleaned_source_orders = get_cleaned_orders(txt_path)
-        parse_export_orders(testing, cleaned_source_orders, txt_path)
+        parse_export_orders(testing, skip_etonas, cleaned_source_orders, txt_path)
         print(VBA_OK)
     else:
         logging.critical(f'Provided file {txt_path} does not exist.')
