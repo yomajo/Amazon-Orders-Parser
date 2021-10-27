33299c733198bed45626d5f007a3d3d5b008c59b
yomajo <ed.vinas@yahoo.com>
2021-10-27 15:13:35 +0300

    updated to support etsy orders -chanes in var names -new util funcs and related imports -accepting addt'l vars on class init to handle etsy orders

--- Helper Files/etonas_xlsx_exporter.py
+++ Helper Files/etonas_xlsx_exporter.py
@@ -1,5 +1,6 @@
+from parser_utils import get_origin_country, get_total_price
+from parser_utils import get_sales_channel_category_brand, get_sales_channel_hs_code
 from parser_constants import ETONAS_HEADERS, ETONAS_HEADERS_MAPPING
-from parser_utils import get_product_category_or_brand, get_hs_code, get_origin_country
 import logging
 import openpyxl
 import sys
@@ -11,10 +12,19 @@ VBA_ETONAS_CHARTLIMIT_ALERT = 'ETONAS_CHARLIMIT_WARNING'
 
 
 class EtonasExporter():
-    '''accepts etonas orders data as list of dicts, creates formatted xlsx output'''
-    def __init__(self, etonas_orders : list, etonas_path : str):
+    '''creates workbook based on Etonas shippment company xlsx requirements
+    
+    Args:
+    -etonas_orders: list of etonas orders (dicts)
+    -etonas_path: workbook path to be saved at
+    -sales_channel: str option AmazonEU / AmazonCOM / Etsy
+    -proxy_keys: dict to handle both Amazon and Etsy sales channels'''
+
+    def __init__(self, etonas_orders : list, etonas_path : str, sales_channel : str, proxy_keys : dict):
         self.etonas_orders = etonas_orders
         self.etonas_path = etonas_path
+        self.sales_channel = sales_channel
+        self.proxy_keys = proxy_keys
     
     def reformat_data_to_etonas_output(self, orders_data : list):
         '''reduces input data to that needed in output csv'''
@@ -28,51 +38,66 @@ class EtonasExporter():
             print(VBA_ERROR_ALERT)
             logging.warning(f'Error while iterating collected row dicts and trying to reduce in ETONAS. Error: {e}')
 
-    def prepare_etonas_order_contents(self, order_dict : dict):
-        d_with_output_keys = {}
-        first_name, last_name = self.get_fname_lname(order_dict)
+    def prepare_etonas_order_contents(self, order : dict):
+        '''returns ready-to-write order data dict based on Etonas file headers'''
+        export = {}
+        first_name, last_name = self._get_fname_lname(order)
+        product_name_proxy_key = self.proxy_keys.get('product-name', '')
         # Change GB to UK for Etonas
-        if order_dict['ship-country'] == 'GB':
-            order_dict['ship-country'] = 'UK'
+        if order[self.proxy_keys['ship-country']] == 'GB':
+            order[self.proxy_keys['ship-country']] = 'UK'
         
         for header in ETONAS_HEADERS:
             if header in ETONAS_HEADERS_MAPPING.keys():
-                d_with_output_keys[header] = order_dict[ETONAS_HEADERS_MAPPING[header]]
+                # Etsy has no phone, email, returning empty string, to prevent KeyError
+                target_key = self.proxy_keys.get(ETONAS_HEADERS_MAPPING[header], '')
+                export[header] = order.get(target_key, '')
+
                 # warn in VBA if char limit per cell is exceeded in Etonas address lines 1/2/3/4
-                if 'address' in header.lower() and len(d_with_output_keys[header]) > ETONAS_CHARLIMIT_PER_CELL:
-                    logging.info(f'Order with key {header} and value {d_with_output_keys[header]} triggered VBA warning for charlimit set by Etonas')
+                if 'address' in header.lower() and len(export[header]) > ETONAS_CHARLIMIT_PER_CELL:
+                    logging.info(f'Order with key {header} and value {export[header]} triggered VBA warning for charlimit set by Etonas')
                     print(VBA_ETONAS_CHARTLIMIT_ALERT)
+            
             elif header == 'First_name':
-                d_with_output_keys['First_name'] = first_name
+                export[header] = first_name
             elif header == 'Last_name':
-                d_with_output_keys['Last_name'] = last_name
+                export[header] = last_name
             elif header == 'Contents':
-                d_with_output_keys['Contents'] = get_product_category_or_brand(order_dict['product-name'])
+                export[header] = get_sales_channel_category_brand(order, product_name_proxy_key)
             elif header == 'HS':
-                item_brand = get_product_category_or_brand(order_dict['product-name'], return_brand=True)
-                item_category = get_product_category_or_brand(order_dict['product-name'])                
-                d_with_output_keys['HS'] = get_hs_code(item_brand, item_category)
+                export[header] = get_sales_channel_hs_code(order, product_name_proxy_key)
             elif header == 'Origin':
-                d_with_output_keys['Origin'] = get_origin_country(order_dict['product-name'])
+                # etsy - no item title, hardcoding for etsy until weight mapping
+                if product_name_proxy_key == '':
+                    export[header] = 'CN'
+                else:
+                    export[header] = get_origin_country(order[product_name_proxy_key])
             elif header == 'Currency':
-                d_with_output_keys['Currency'] = order_dict['currency'].lower()
+                target_key = self.proxy_keys['currency']
+                export[header] = order[target_key].lower()
+            elif header == 'Price per quantity':
+                export[header] = get_total_price(order, self.sales_channel)
             else:
-                d_with_output_keys[header] = ''
-        return d_with_output_keys
+                export[header] = ''
+        return export
     
-    @staticmethod
-    def get_fname_lname(order : dict):
-        '''returns first and last name split at space in order dict key ['recipient-name']'''
+    def _get_fname_lname(self, order : dict):
+        '''returns first and last name based on sales channel'''
         try:
-            f_name, l_name = order['recipient-name'].split(' ', 1)
-            return f_name, l_name
+            if self.sales_channel == 'Etsy':
+                f_name = order[self.proxy_keys['buyer-fname']]
+                l_name = order[self.proxy_keys['buyer-lname']]
+                return f_name, l_name
+            else:
+                f_name, l_name = order[self.proxy_keys['recipient-name']].split(' ', 1)
+                return f_name, l_name
         except KeyError as e:
-            logging.critical(f'No recipient-name key. Error: {e} Order: {order}')
+            logging.critical(f'No recipient-name key for etonas func: _get_fname_lname. Err: {e} Order: {order}')
             print(VBA_ERROR_ALERT)
             sys.exit()
         except ValueError as e:
-            logging.info(f'recipient-name seems to be one word, without a need to unpack. Error: {e}. recipient-name: {order["recipient-name"]}. Returning empty last_name')
-            return order['recipient-name'], ''
+            logging.info(f'Failed to unpack f_name, l_name for sales ch: {self.sales_channel} etonas xlsx. Err: {e}. Returning proxy recipient-name order val: {order[self.proxy_keys["recipient-name"]]} and empty l_name')
+            return order[self.proxy_keys['recipient-name']], ''
 
     @staticmethod
     def _write_headers(worksheet, headers):
